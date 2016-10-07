c0383cf08dc3effac3e411bdbaa82688e0417662
Hack Reactor Students <accounts@hackreactor.com>
2016-10-06 19:54:18 -0700

    Make initial files.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,20 @@
 class App extends React.Component {
-  
+   constructor(props) {
+     super(props); 
+
+     this.state = {
+
+     };
+   }
+
+   render() {
+    return (
+      <div>
+        Hello! :-)
+        <PhotoEntry props={} />
+      </div>
+      );
+   }
 }
 
 window.App = App;
---
+++ src/components/PhotoEntry.jsx
@@ -0,0 +1,17 @@
+class PhotoEntry extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>Text Goes Here!</div>
+    );
+  }
+};
+
+window.PhotoEntry = PhotoEntry;
